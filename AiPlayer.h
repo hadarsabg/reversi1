6714e6e6aa0d3609b8b6478c1c190cf635f4e0d3
inbalzelinger <inbal.zelinger@gmail.com>
2017-11-23 18:45:26 +0200

    crate Ai class

---
+++ AiPlayer.h
@@ -0,0 +1,19 @@
+//
+// Created by inbal on 23/11/17.
+//
+
+#ifndef UNTITLED1_AIPLAYER_H
+#define UNTITLED1_AIPLAYER_H
+
+
+#include "Player.h"
+
+class AiPlayer: public Player {
+    AiPlayer(char symbol);
+    BoardCell* playOneTurn(vector<BoardCell *>& possibleMoves);
+
+
+};
+
+
+#endif //UNTITLED1_AIPLAYER_H
